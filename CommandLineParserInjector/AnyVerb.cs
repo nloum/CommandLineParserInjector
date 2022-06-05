47410977d21b7b046decb4493ade821f1c1e98e0
Nathan Loum <nathan.loum@amreact.com>
2022-06-05 05:56:19 -0500

    Improve documentation

--- CommandLineParserInjector/AnyVerb.cs
+++ CommandLineParserInjector/AnyVerb.cs
@@ -1,6 +1,10 @@
 namespace CommandLineParserInjector;
 
+/// <summary>
+/// A class that contains the verb object, no matter which verb was specified on the command line
+/// </summary>
 public class AnyVerb
 {
+    // The verb object, no matter which verb was specified on the command line
     public object Value { get; init; }
 }
